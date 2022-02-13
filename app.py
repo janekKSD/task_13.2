e34052877726f6e1107608ff022b662afc0c0668
Michał Jankowski <jankowski.nml@gmail.com>
2022-02-13 20:46:11 +0100

    Beta 1.0

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, make_response, render_template, redirect, url_for, jsonify, abort
+
+from forms import BookForm
+from models import books
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "nininini"
+
+@app.route('/books/', methods=["GET", "POST"])
+def books_list():
+    form = BookForm()
+    error = ""
+    if request.method == "POST":
+        print('if method post')
+        if form.validate_on_submit():
+            books.create(form.data)
+        return redirect(url_for("books_list"))
+    
+    return render_template("books.html", form=form, books=books.all(), error=error)
+
+@app.route("/book/<int:book_id>/", methods=["GET", "POST"])
+def book_details(book_id):
+    book = books.get(book_id)
+    print(tuple(book))
+    form = BookForm(data=book)
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            books.update(book_id, form.data)
+        return redirect(url_for("books_list"))
+    return render_template("book.html", form=form, book_id=book_id)
+
+if __name__ == "__main__":
+    app.run(debug=True)
